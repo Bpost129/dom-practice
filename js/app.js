85ddd71898ef20f5581c303d80e1d33a8e2ec5a4
Bpost129 <Bpost129@yahoo.com>
2024-10-31 12:53:22 -0400

    add title element, create element reference and edit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-let color = 'green'
+const titleEl = document.querySelector('#main-title')
+titleEl.style.textAlign = 'center'
+titleEl.style.backgroundColor = 'coral'
+
+const imgEl = document.querySelector('#imagee')
+
+
+// console.log(titleEl)
