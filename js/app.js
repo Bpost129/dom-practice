1da0f3dfd69ccd8a368704e64a8614b41d084c5f
Bpost129 <Bpost129@yahoo.com>
2024-10-31 23:24:48 -0400

    format files for dom event practice

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-const titleEl = document.querySelector('#main-title')
-titleEl.style.textAlign = 'center'
-titleEl.style.backgroundColor = 'coral'
-
-const imgEl = document.querySelector('#imagee')
-
-imgEl.setAttribute("src", `https://picsum.photos/${getRandomInt(200, 800)}`)
-imgEl.setAttribute("alt", "A random placeholder image")
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
