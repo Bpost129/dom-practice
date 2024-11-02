7242601096cf8f4118d05380ffd484077116fb87
Bpost129 <Bpost129@yahoo.com>
2024-11-02 00:12:00 -0400

    clean up code and add line breaks for clarity

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+/* --------------- Variables ------------- */
+
 let likesCount = 0
+
+/* ----------- Element References --------- */
 
 const likeBtnEl = document.querySelector('#like-button')
 const dislikeBtnEl = document.querySelector('#dislike-button')
 const commentBtnEl = document.querySelector('#comment-button')
 const commentListEl = document.querySelector('ul')
 const inputEl = document.querySelector('input')
+
+/* --------------- Functions ------------- */
 
 const submitComment = () => {
   const commentEl = document.createElement('li');
@@ -18,7 +24,10 @@ const handleReaction = (e) => {
   likeBtnEl.textContent = `${likesCount} like(s). Like this post!`
 }
 
+/* ------------- Event Listenters ----------- */
+
 likeBtnEl.addEventListener('click', handleReaction)
+
 dislikeBtnEl.addEventListener('click', handleReaction)
 
 commentBtnEl.addEventListener('click', submitComment)
@@ -27,7 +36,3 @@ inputEl.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') commentBtnEl.click()
 })
 
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
