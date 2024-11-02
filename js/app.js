694b99520e1d916d2956c8a9df0accb9b4f7fa19
Bpost129 <Bpost129@yahoo.com>
2024-11-01 23:57:06 -0400

    create helper function for likes button event listener, display likes count on button

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
+let likesCount = 0
+
 const likeBtnEl = document.querySelector('#like-button')
 const commentBtnEl = document.querySelector('#comment-button')
 const commentListEl = document.querySelector('ul')
 const inputEl = document.querySelector('input')
-
-// console.dir(inputEl)
-likeBtnEl.addEventListener('click', () => {
-  console.log('You clicked me!')
-})
 
 const submitComment = () => {
   const commentEl = document.createElement('li');
   commentEl.textContent = inputEl.value
   inputEl.value = ''
   if (commentEl.textContent !== '') commentListEl.appendChild(commentEl)
+  }
+
+const handleLike = () => {
+  likesCount++
+  likeBtnEl.textContent = `${likesCount} like(s). Like this post!`
 }
 
+likeBtnEl.addEventListener('click', handleLike)
+
 commentBtnEl.addEventListener('click', submitComment)
+
 inputEl.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') commentBtnEl.click()
 })
