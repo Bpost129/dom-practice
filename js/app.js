1e80eee8ba73eec011b9ba9bf21b6dad8ba00d7b
Bpost129 <Bpost129@yahoo.com>
2024-11-01 22:51:54 -0400

    add element reference for like button and click event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+const likeBtnEl = document.querySelector('#like-button')
+// console.dir(likeBtnEl)
+likeBtnEl.addEventListener('click', () => {
+  console.log('You clicked me!')
+})
+
+
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
