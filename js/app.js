808678240c3841891be28cfca10d2aff9fb91424
Bpost129 <Bpost129@yahoo.com>
2024-11-01 23:26:20 -0400

    add element reference for add comment button, add event listener

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const likeBtnEl = document.querySelector('#like-button')
-// console.dir(likeBtnEl)
+const commentBtnEl = document.querySelector('#comment-button')
+
+// console.dir(commentBtnEl)
 likeBtnEl.addEventListener('click', () => {
   console.log('You clicked me!')
 })
 
-
+commentBtnEl.addEventListener('click', () => {
+  console.log('I work!!')
+})
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
