e235dc96a29bbdb857b020da5c1fd5bf7598c5a7
Bpost129 <Bpost129@yahoo.com>
2024-11-01 23:35:43 -0400

    add element reference to comment input, change comments from hard coded to input value

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 const likeBtnEl = document.querySelector('#like-button')
 const commentBtnEl = document.querySelector('#comment-button')
+const commentListEl = document.querySelector('ul')
+const inputEl = document.querySelector('input')
 
-// console.dir(commentBtnEl)
+// console.dir(inputEl)
 likeBtnEl.addEventListener('click', () => {
   console.log('You clicked me!')
 })
 
 commentBtnEl.addEventListener('click', () => {
-  console.log('I work!!')
+  const commentEl = document.createElement('li');
+  commentEl.textContent = inputEl.value
+  commentListEl.appendChild(commentEl)
 })
+
+
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
